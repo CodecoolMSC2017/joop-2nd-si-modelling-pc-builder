da5c7f79b7b2518a69ee84cda41a0264e634bb24
Gyorfisti <gyorfisti@gmail.com>
2018-01-15 21:13:09 +0100

    swapped Electronic and PCComponent classes in hierarchy

--- src/com/codecool/PCComponent.java
+++ src/com/codecool/PCComponent.java
@@ -1,16 +1,33 @@
 package com.codecool;
 
-public class PCComponent extends Electronic {
+public class PCComponent {
 
-    private Tier tier;
+    private String name;
+    private String manufacturer;
+    private int powerConsumption;
+    private int value;
 
-    public PCComponent(Tier tier, String name, String manufacturer, int powerConsumption, int value) {
-        super(name, manufacturer, powerConsumption, value);
-        this.tier = tier;
+    public PCComponent(String name, String manufacturer, int powerConsumption, int value) {
+        this.name = name;
+        this.manufacturer = manufacturer;
+        this.powerConsumption = powerConsumption;
+        this.value = value;
     }
 
-    public Tier getTier() {
-        return tier;
+    public String getName() {
+        return name;
+    }
+
+    public String getManufacturer() {
+        return manufacturer;
+    }
+
+    public int getPowerConsumption() {
+        return powerConsumption;
+    }
+
+    public int getValue() {
+        return value;
     }
 
 }
