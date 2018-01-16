03fd6f5ddedf5e70a1a8512c13e5dfc8a6fc16a7
Gyorfisti <gyorfisti@gmail.com>
2018-01-16 22:38:41 +0100

    Main prints the arrays containing the items of the store for testing

--- src/com/codecool/Main.java
+++ src/com/codecool/Main.java
@@ -1,13 +1,23 @@
 package com.codecool;
 
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
 
     private static Scanner userInput = new Scanner(System.in);
 
     public static void main(String[] args) {
-        
+        Store store = new Store();
+        System.out.println(Arrays.toString(store.getCases()));
+        System.out.println(Arrays.toString(store.getPsus()));
+        System.out.println(Arrays.toString(store.getMotherboards()));
+        System.out.println(Arrays.toString(store.getCpus()));
+        System.out.println(Arrays.toString(store.getHeatsinks()));
+        System.out.println(Arrays.toString(store.getFans()));
+        System.out.println(Arrays.toString(store.getMemories()));
+        System.out.println(Arrays.toString(store.getGpus()));
+        System.out.println(Arrays.toString(store.getSsds()));
+        System.out.println(Arrays.toString(store.getHdds()));
     }
 
 }
