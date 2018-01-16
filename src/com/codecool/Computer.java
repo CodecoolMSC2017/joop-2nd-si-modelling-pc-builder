0b79414039d8083df374211129d4d8f693694bc0
Gyorfisti <gyorfisti@gmail.com>
2018-01-16 15:00:45 +0100

    added 2 field variables to Computer class that will be needed

--- src/com/codecool/Computer.java
+++ src/com/codecool/Computer.java
@@ -5,16 +5,20 @@ public class Computer {
     private Case casing;
     private PowerSupply psu;
     private Motherboard motherboard;
+    private Heatsink heatsink;
+    private Fan[] fans;
     private CPU[] cpu;
     private Memory[] ram;
     private GraphicsCard[] gpu;
     private Storage[] storage;
 
-    public Computer(Case casing, PowerSupply psu, Motherboard motherboard, CPU[] cpu,
-    Memory[] ram, GraphicsCard[] gpu, Storage[] storage) {
+    public Computer(Case casing, PowerSupply psu, Motherboard motherboard, Heatsink heatsink,
+    Fan[] fans, CPU[] cpu, Memory[] ram, GraphicsCard[] gpu, Storage[] storage) {
         this.casing = casing;
         this.psu = psu;
         this.motherboard = motherboard;
+        this.heatsink = heatsink;
+        this.fans = fans;
         this.cpu = cpu;
         this.ram = ram;
         this.gpu = gpu;
