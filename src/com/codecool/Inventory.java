224fb725f01b1289bf2eea351f9284472e99e590
Gyorfisti <gyorfisti@gmail.com>
2018-01-18 10:16:28 +0100

    items can now be added to inventory (not tested yet, but it compiles)

--- src/com/codecool/Inventory.java
+++ src/com/codecool/Inventory.java
@@ -28,6 +28,156 @@ public class Inventory {
         this.hdds = hdds;
     }
 
+    public void addItem(Case item) {
+        Case[] newArray = new Case[cases.length + 1];
+        int counter = 0;
+        for (Case aCase : cases) {
+            newArray[counter] = aCase;
+            counter++;
+        }
+        newArray[cases.length] = item;
+        cases = newArray;
+    }
+
+    public void addItem(PowerSupply item) {
+        PowerSupply[] newArray = new PowerSupply[psus.length + 1];
+        int counter = 0;
+        for (PowerSupply psu : psus) {
+            newArray[counter] = psu;
+            counter++;
+        }
+        newArray[psus.length] = item;
+        psus = newArray;
+    }
+
+    public void addItem(Motherboard item) {
+        Motherboard[] newArray = new Motherboard[motherboards.length + 1];
+        int counter = 0;
+        for (Motherboard motherboard : motherboards) {
+            newArray[counter] = motherboard;
+            counter++;
+        }
+        newArray[motherboards.length] = item;
+        motherboards = newArray;
+    }
+
+    public void addItem(CPU item) {
+        CPU[] newArray = new CPU[cpus.length + 1];
+        int counter = 0;
+        for (CPU cpu : cpus) {
+            newArray[counter] = cpu;
+            counter++;
+        }
+        newArray[cpus.length] = item;
+        cpus = newArray;
+    }
+
+    public void addItem(Heatsink item) {
+        Heatsink[] newArray = new Heatsink[heatsinks.length + 1];
+        int counter = 0;
+        for (Heatsink heatsink : heatsinks) {
+            newArray[counter] = heatsink;
+            counter++;
+        }
+        newArray[heatsinks.length] = item;
+        heatsinks = newArray;
+    }
+
+    public void addItem(Fan item) {
+        Fan[] newArray = new Fan[fans.length + 1];
+        int counter = 0;
+        for (Fan fan : fans) {
+            newArray[counter] = fan;
+            counter++;
+        }
+        newArray[fans.length] = item;
+        fans = newArray;
+    }
+
+    public void addItem(Memory item) {
+        Memory[] newArray = new Memory[memories.length + 1];
+        int counter = 0;
+        for (Memory memory : memories) {
+            newArray[counter] = memory;
+            counter++;
+        }
+        newArray[memories.length] = item;
+        memories = newArray;
+    }
+
+    public void addItem(GraphicsCard item) {
+        GraphicsCard[] newArray = new GraphicsCard[gpus.length + 1];
+        int counter = 0;
+        for (GraphicsCard gpu : gpus) {
+            newArray[counter] = gpu;
+            counter++;
+        }
+        newArray[gpus.length] = item;
+        gpus = newArray;
+    }
+
+    public void addItem(SolidStateDrive item) {
+        SolidStateDrive[] newArray = new SolidStateDrive[ssds.length + 1];
+        int counter = 0;
+        for (SolidStateDrive ssd : ssds) {
+            newArray[counter] = ssd;
+            counter++;
+        }
+        newArray[ssds.length] = item;
+        ssds = newArray;
+    }
+
+    public void addItem(HardDiskDrive item) {
+        HardDiskDrive[] newArray = new HardDiskDrive[hdds.length + 1];
+        int counter = 0;
+        for (HardDiskDrive hdd : hdds) {
+            newArray[counter] = hdd;
+            counter++;
+        }
+        newArray[hdds.length] = item;
+        hdds = newArray;
+    }
+
+    public void deleteItem(Case item) {
+
+    }
+
+    public void deleteItem(PowerSupply item) {
+
+    }
+
+    public void deleteItem(Motherboard item) {
+
+    }
+
+    public void deleteItem(CPU item) {
+
+    }
+
+    public void deleteItem(Heatsink item) {
+
+    }
+
+    public void deleteItem(Fan item) {
+
+    }
+
+    public void deleteItem(Memory item) {
+
+    }
+
+    public void deleteItem(GraphicsCard item) {
+
+    }
+
+    public void deleteItem(SolidStateDrive item) {
+
+    }
+
+    public void deleteItem(HardDiskDrive item) {
+
+    }
+
     public Case[] getCases() {
         return cases;
     }
