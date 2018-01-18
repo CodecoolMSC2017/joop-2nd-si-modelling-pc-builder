8de708d229fdef25aa92b53bbd1b6e6ee554648f
Gyorfisti <gyorfisti@gmail.com>
2018-01-18 09:52:07 +0100

    added colors to displayInventory based on tier, started working on purchasing from store

--- src/com/codecool/Main.java
+++ src/com/codecool/Main.java
@@ -27,7 +27,7 @@ public class Main {
                 break;
             } else
             if (input.equals(":inventory")) {
-                displayInventory(store, "Store Menu");
+                inventoryMenu(inventory);
             } else
             if (input.equals(":save")) {
                 break;
@@ -46,72 +46,120 @@ public class Main {
     }
 
     static void storeMenu(Store store) {
-        System.out.println("Store Menu\nCommands: :back");
+        while (true) {
+            String input = displayInventory(store, "Store Menu", ":back (or type corresponding number");
+            if (input.equals(":back")) {
+                return;
+            }
+            System.out.println("\nCommands: :purchase :back\n");
+            input = userInput.nextLine();
+            if (input.equals(":back")) {
+                continue;
+            }
+            if (!input.equals(":purchase")) {
+                System.out.println("\nWrong input!\n");
+                continue;
+            }
+            System.out.println("Select an item by it's number:");
+            input = userInput.nextLine();
+            try {
+                int inputInNumber = Integer.parseInt(input);
+            } catch(NumberFormatException e) {
+                System.out.println("\nWrong input!\n");
+                continue;
+            }
+            
+        }
+    }
+
+    static void inventoryMenu(UserInventory inventory) {
+        while (true) {
+            String input = displayInventory(inventory, "Inventory Menu", ":back (or type corresponding number");
+            if (input.equals(":back")) {
+                break;
+            }
+        }
     }
 
     static void buildMenu(Inventory inventory) {
         System.out.println("this is the build menu");
     }
 
-    static void displayInventory(Inventory inventory, String menuTitle) {
-        while (true) {
-            System.out.println("\n" + menuTitle + "\nCommands: :back (or type corresponding number)\n");
-            System.out.println("0 (" + inventory.getCases().length + " items) Cases");
-            System.out.println("1 (" + inventory.getPsus().length + " items) Power supplies");
-            System.out.println("2 (" + inventory.getMotherboards().length + " items) Motherboards");
-            System.out.println("3 (" + inventory.getCpus().length + " items) Processors");
-            System.out.println("4 (" + inventory.getHeatsinks().length + " items) Heatsinks");
-            System.out.println("5 (" + inventory.getFans().length + " items) Fans");
-            System.out.println("6 (" + inventory.getMemories().length + " items) Memories");
-            System.out.println("7 (" + inventory.getGpus().length + " items) Graphics cards");
-            System.out.println("8 (" + inventory.getSsds().length + " items) Solid state drives");
-            System.out.println("9 (" + inventory.getHdds().length + " items) Hard disk drives\n");
-            String input = userInput.nextLine().toLowerCase();
+    static String displayInventory(Inventory inventory, String menuTitle, String commands) {
+        System.out.println("\n" + menuTitle + "\nCommands: " + commands + "\n");
+        System.out.println("0 (" + inventory.getCases().length + " items) Cases");
+        System.out.println("1 (" + inventory.getPsus().length + " items) Power supplies");
+        System.out.println("2 (" + inventory.getMotherboards().length + " items) Motherboards");
+        System.out.println("3 (" + inventory.getCpus().length + " items) Processors");
+        System.out.println("4 (" + inventory.getHeatsinks().length + " items) Heatsinks");
+        System.out.println("5 (" + inventory.getFans().length + " items) Fans");
+        System.out.println("6 (" + inventory.getMemories().length + " items) Memories");
+        System.out.println("7 (" + inventory.getGpus().length + " items) Graphics cards");
+        System.out.println("8 (" + inventory.getSsds().length + " items) Solid state drives");
+        System.out.println("9 (" + inventory.getHdds().length + " items) Hard disk drives\n");
 
-            if (input.equals(":back")) {
-                return;
-            }
-            if (input.equals("0")) {
-                displayItems(inventory.getCases());
-            } else
-            if (input.equals("1")) {
-                displayItems(inventory.getPsus());
-            } else
-            if (input.equals("2")) {
-                displayItems(inventory.getMotherboards());
-            } else
-            if (input.equals("3")) {
-                displayItems(inventory.getCpus());
-            } else
-            if (input.equals("4")) {
-                displayItems(inventory.getHeatsinks());
-            } else
-            if (input.equals("5")) {
-                displayItems(inventory.getFans());
-            } else
-            if (input.equals("6")) {
-                displayItems(inventory.getMemories());
-            } else
-            if (input.equals("7")) {
-                displayItems(inventory.getGpus());
-            } else
-            if (input.equals("8")) {
-                displayItems(inventory.getSsds());
-            } else
-            if (input.equals("9")) {
-                displayItems(inventory.getHdds());
-            }
+        String input = userInput.nextLine().toLowerCase();
+
+        if (input.equals(":back")) {
+            return input;
         }
+        if (input.equals("0")) {
+            displayItems(inventory.getCases());
+        } else
+        if (input.equals("1")) {
+            displayItems(inventory.getPsus());
+        } else
+        if (input.equals("2")) {
+            displayItems(inventory.getMotherboards());
+        } else
+        if (input.equals("3")) {
+            displayItems(inventory.getCpus());
+        } else
+        if (input.equals("4")) {
+            displayItems(inventory.getHeatsinks());
+        } else
+        if (input.equals("5")) {
+            displayItems(inventory.getFans());
+        } else
+        if (input.equals("6")) {
+            displayItems(inventory.getMemories());
+        } else
+        if (input.equals("7")) {
+            displayItems(inventory.getGpus());
+        } else
+        if (input.equals("8")) {
+            displayItems(inventory.getSsds());
+        } else
+        if (input.equals("9")) {
+            displayItems(inventory.getHdds());
+        }
+        return input;
     }
 
     static void displayItems(PCComponent[] items) {
         if (items.length < 1) {
-            System.out.println("You don't have any components of this type.");
+            System.out.println("\nYou don't have any components of this type.");
             return;
         }
+        int counter = 0;
+        String colorCode;
         System.out.println();
         for (PCComponent item : items) {
-            System.out.println(item);
+            switch(item.getTier()) {
+                case HIGH:
+                    colorCode = "\033[91m";
+                    break;
+                case MEDIUM:
+                    colorCode = "\033[93m";
+                    break;
+                case LOW:
+                    colorCode = "\033[92m";
+                    break;
+                default:
+                    colorCode = "\033[0m";
+            }
+            System.out.println(counter + " " + colorCode + item + "\033[0m");
+            counter++;
         }
     }
 
